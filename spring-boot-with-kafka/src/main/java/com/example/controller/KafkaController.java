6364e70639e8a9d166091270bf450abd48c2c15e
Kaliappan <kaliappan4u@gmail.com>
2018-11-28 02:07:21 +0800

    initial commit

---
+++ spring-boot-with-kafka/src/main/java/com/example/controller/KafkaController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.engine.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+
+    private final Producer producer;
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
+
